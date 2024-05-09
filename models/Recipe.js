03e9a545ba22620469c4a71d491283c64cc72200
UltimaVanguard <ultimavanguard@gmail.com>
2024-05-09 13:10:09 -0500

    added recipe model

---
+++ models/Recipe.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Recipe extends Model {}
+
+Recipe.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        instructions: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        ingredients: {
+            type: DataTypes.ARRAY,
+            allowNull: false,
+        },
+        image: {
+            type: DataTypes.STRING,
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'recipe',
+    }
+);
+
+module.exports = Recipe;
