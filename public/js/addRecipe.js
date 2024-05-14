16f61c7b771adffda56acdd194a2e4d3d8d9b4a8
CLBlue60 <clblue@yahoo.com>
2024-05-14 10:28:19 -0500

    fixed button functionality

--- public/js/addRecipe.js
+++ public/js/addRecipe.js
@@ -20,22 +20,4 @@ document
   .querySelector('.button-recipe')
   .addEventListener('click', addRecipe);
 
-  var myWidget = cloudinary.createUploadWidget(
-    {
-      cloudName: "drpv4wdcd",
-      uploadPreset: "ahulubyv",
-    },
-    (error, result) => {
-      if (!error && result && result.event === "success") {
-        console.log("Done! Here is the image info: ", result.info);
-      }
-    }
-  );
-
-  document.getElementById("upload_widget").addEventListener(
-    "click",
-    function () {
-      myWidget.open();
-    },
-    false
-  );
+ 
