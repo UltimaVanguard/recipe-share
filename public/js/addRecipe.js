4d610d5425a885589e9f6550d621d111536d2c39
CLBlue60 <clblue@yahoo.com>
2024-05-14 09:51:28 -0500

    push uP

--- public/js/addRecipe.js
+++ public/js/addRecipe.js
@@ -8,7 +8,7 @@ const addRecipe = async (event) => {
         headers: { 'Content-Type': 'application/json' },
     });
 
-      
+
     if (response.ok) {
         document.location.replace('/profile');
     } else {
@@ -19,3 +19,23 @@ const addRecipe = async (event) => {
 document
   .querySelector('.button-recipe')
   .addEventListener('click', addRecipe);
+
+  var myWidget = cloudinary.createUploadWidget(
+    {
+      cloudName: "drpv4wdcd",
+      uploadPreset: "ahulubyv",
+    },
+    (error, result) => {
+      if (!error && result && result.event === "success") {
+        console.log("Done! Here is the image info: ", result.info);
+      }
+    }
+  );
+
+  document.getElementById("upload_widget").addEventListener(
+    "click",
+    function () {
+      myWidget.open();
+    },
+    false
+  );
